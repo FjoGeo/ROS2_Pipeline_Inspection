92f798d54addccb488a2c00fca769ba2119a03c5
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-07-04 10:44:07 +0200

    Update test_launch.py

--- launch/test_launch.py
+++ launch/test_launch.py
@@ -7,15 +7,28 @@ def generate_launch_description():
         Node(
             package='rp_test',
             executable='talker',
-            name='talker_node'
+            name='rplidar_talker'
         ),
         Node(
             package='witmotion_imu',
             executable='talker',
-            name='talker_node2'
+            name='witmotion_imu_talker'
         ),
+
+        Node(
+            package='my_realsense',
+            executable='talker',
+            name='my_realsense_talker'
+        ),
+
         ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '-o', 'my_bag', '/lidar_scan /serial_data'],
+            cmd=['ros2', 'bag', 'record', '-o', 'my_bag', 
+                 'lidar_quality',  'lidar_angle', 'lidar_distance', 
+                 '/serial_data/AccX', '/serial_data/AccY', '/serial_data/AccZ', 
+                 '/serial_data/AngX', '/serial_data/AngY', '/serial_data/AngZ',
+                 '/serial_data/AsX', '/serial_data/AsY', '/serial_data/AsZ',
+                 '/serial_data/HX', '/serial_data/HY', '/serial_data/HZ',
+                 '/realsense/rgb', '/realsense/depth', '/realsense/pointcloud',],
             output='screen'
         )
  
