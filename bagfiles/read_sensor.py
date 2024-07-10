941d14438e206bc67b253da8b71d0c3ee58af13a
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-07-10 11:23:46 +0200

    Update read_sensor.py

--- bagfiles/read_sensor.py
+++ bagfiles/read_sensor.py
@@ -1,36 +1,44 @@
 import sqlite3
 from rclpy.serialization import deserialize_message
-from std_msgs.msg import Int32, Float32, String
-from sensor_msgs.msg import Image, PointCloud2, PointField
+from std_msgs.msg import Int32, Float32
 import rclpy
 import pandas as pd
-import numpy as np
 
 
-# Initialize rclpy
-rclpy.init()
+class SensorReader:
+    def __init__(self, dbPath, topicID):
+        self.dbPath = dbPath
+        self.conn = sqlite3.connect(dbPath)
+        self.cursor = self.conn.cursor()
+        self.rclpy = rclpy
+        self.rclpy.init()
+        self.dataframe = None
+        self.topicID = topicID
+        self.deserializeDataFrame()
+        
 
-bag_file = "my_bag_1/my_bag_0.db3"
-conn = sqlite3.connect(bag_file)
-cur = conn.cursor()
+    def getDataFrame(self):
+        df = pd.read_sql_query(f"SELECT * FROM messages WHERE topic_id = {self.topicID}", self.conn)
+        self.conn.close()
+        return df
 
-df = pd.read_sql_query("SELECT * FROM messages WHERE topic_id = 1 LIMIT 100", conn)
 
-# Function to deserialize data
-def deserialize_data(data, msg_type):
-    return deserialize_message(data, msg_type)
+    def deserializeDataFrame(self):
+        df = self.getDataFrame()
+        df['deserialized_data'] = df['data'].apply(lambda x: deserialize_message(x, Float32))
+        df['deserialized_data'] = df['deserialized_data'].apply(lambda msg: msg.data)
+        self.dataframe = df[['timestamp', 'deserialized_data']]
+        self.dataframe.columns = ['timestamp', 'data']
+        self.rclpy.shutdown()
 
-# Apply the deserialization to the "data" column
-df['deserialized_data'] = df['data'].apply(lambda x: deserialize_data(x, Float32))
 
-# Extract the actual data from the deserialized messages
-df['deserialized_data'] = df['deserialized_data'].apply(lambda msg: msg.data)
+    def printDataFrame(self):
+        print(self.dataframe.head())
+        print(self.dataframe.tail())
 
-# Display the deserialized data
-print(df[['timestamp', 'deserialized_data']])
 
-# Close the connection
-conn.close()
-
-# Shutdown rclpy
-rclpy.shutdown()
+if __name__ == "__main__":
+    dbPath = "my_bag_1/my_bag_0.db3"
+    topicID = 1
+    sensor = SensorReader(dbPath, topicID)
+    sensor.printDataFrame()
