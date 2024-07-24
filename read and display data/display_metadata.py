50ecaae177f1cb01ac1c5de8b6570529474603d5
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-07-24 13:34:41 +0200

    Update display_metadata.py

--- read and display data/display_metadata.py
+++ read and display data/display_metadata.py
@@ -28,11 +28,6 @@ class DisplayMetaData:
         df = pd.read_sql_query("""SELECT 
                                         id, name, type 
                                     FROM topics 
-                                        where 
-                                    name LIKE '%HZ' OR name LIKE '%HY' OR name LIKE '%HX' 
-                                    OR name LIKE '%AccX' OR name LIKE '%AccY' OR name LIKE '%AccZ'
-                                    OR name LIKE '%AsY' OR name LIKE '%AsX' OR name LIKE '%AsZ'
-                                    OR name LIKE '%AngX' OR name LIKE '%AngY' OR name LIKE '%AngZ'
                                     """, self.conn)
 
         df['name'] = df['name'].str.rsplit('/').str[-1]
