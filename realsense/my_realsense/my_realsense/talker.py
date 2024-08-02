5a0e6a8c8de1eae236c0449b1aa02bef04e1330d
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-08-02 13:49:40 +0200

    Update talker.py

--- realsense/my_realsense/my_realsense/talker.py
+++ realsense/my_realsense/my_realsense/talker.py
@@ -3,7 +3,7 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image, PointCloud2, PointField
-from std_msgs.msg import Header
+from std_msgs.msg import Header, Float32, String, Float32MultiArray
 from cv_bridge import CvBridge
 import pyrealsense2 as rs
 import numpy as np
@@ -17,11 +17,15 @@ class RealSensePublisher(Node):
         self.rgb_publisher1 = self.create_publisher(Image, 'realsense1/rgb', 10)
         self.depth_publisher1 = self.create_publisher(Image, 'realsense1/depth', 10)
         self.pc_publisher1 = self.create_publisher(PointCloud2, 'realsense1/pointcloud', 10)
+        self.accel_publisher1 = self.create_publisher(Float32MultiArray, 'realsense1/accel', 10)
+        self.gyro_publisher1 = self.create_publisher(Float32MultiArray, 'realsense1/gyro', 10)
 
         # Create publishers for camera 2
         self.rgb_publisher2 = self.create_publisher(Image, 'realsense2/rgb', 10)
         self.depth_publisher2 = self.create_publisher(Image, 'realsense2/depth', 10)
         self.pc_publisher2 = self.create_publisher(PointCloud2, 'realsense2/pointcloud', 10)
+        self.accel_publisher2 = self.create_publisher(Float32MultiArray, 'realsense2/accel', 10)
+        self.gyro_publisher2 = self.create_publisher(Float32MultiArray, 'realsense2/gyro', 10)
 
         self.timer = self.create_timer(0.1, self.timer_callback)  # Publish at 10Hz
         self.bridge = CvBridge()
@@ -36,29 +40,52 @@ class RealSensePublisher(Node):
         self.pipeline1.start(self.config1)
         self.pipeline2.start(self.config2)
 
+
+    @staticmethod
+    def gyro_data(gyro):
+        return np.array([gyro.x, gyro.y, gyro.z])
+    
+
+    @staticmethod
+    def accel_data(accel):
+        return np.array([accel.x, accel.y, accel.z])
+
+
     def configure_camera(self, serial_number):
         pipeline = rs.pipeline()
         config = rs.config()
+        config.enable_stream(rs.stream.accel)
+        config.enable_stream(rs.stream.gyro)
         config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
         config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
         config.enable_device(serial_number)
         return pipeline, config
 
+
     def timer_callback(self):
         # Get frames from camera 1
         frames1 = self.pipeline1.wait_for_frames()
+        accel1 = self.accel_data(frames1[2].as_motion_frame().get_motion_data())
+        gyro1 = self.gyro_data(frames1[3].as_motion_frame().get_motion_data())
         depth_frame1 = frames1.get_depth_frame()
         color_frame1 = frames1.get_color_frame()
 
         # Get frames from camera 2
         frames2 = self.pipeline2.wait_for_frames()
+        accel2 = self.accel_data(frames2[2].as_motion_frame().get_motion_data())
+        gyro2 = self.gyro_data(frames2[3].as_motion_frame().get_motion_data())
         depth_frame2 = frames2.get_depth_frame()
         color_frame2 = frames2.get_color_frame()
 
         if depth_frame1 and color_frame1:
-            self.publish_data(depth_frame1, color_frame1, self.rgb_publisher1, self.depth_publisher1, self.pc_publisher1, "camera1_link")
+            self.publish_data(depth_frame1, color_frame1, self.rgb_publisher1, self.depth_publisher1, self.pc_publisher1, "camera1")
         if depth_frame2 and color_frame2:
-            self.publish_data(depth_frame2, color_frame2, self.rgb_publisher2, self.depth_publisher2, self.pc_publisher2, "camera2_link")
+            self.publish_data(depth_frame2, color_frame2, self.rgb_publisher2, self.depth_publisher2, self.pc_publisher2,  "camera2")
+        if accel1.any() and gyro1.any():
+            self.publish_accel_gyro(accel1, gyro1, self.accel_publisher1, self.gyro_publisher1, "camera1")
+        if accel2.any() and gyro2.any():
+            self.publish_accel_gyro(accel2, gyro2, self.accel_publisher2, self.gyro_publisher2, "camera2")
+
 
     def publish_data(self, depth_frame, color_frame, rgb_publisher, depth_publisher, pc_publisher, frame_id):
         # Convert images to numpy arrays
@@ -78,6 +105,20 @@ class RealSensePublisher(Node):
         pc_publisher.publish(points)
 
         self.get_logger().info(f'Published RGB, depth, and point cloud data for {frame_id}')
+
+
+    def publish_accel_gyro(self, accel, gyro, accel_publisher, gyro_publisher, frame_id):
+        accel_msg = Float32MultiArray()
+        accel_msg.data = [accel[0], accel[1], accel[2]]
+        accel_publisher.publish(accel_msg)
+
+        gyro_msg = Float32MultiArray()
+        gyro_msg.data = [gyro[0], gyro[1], gyro[2]]
+        gyro_publisher.publish(gyro_msg)
+
+        self.get_logger().info(f'{frame_id} Accelerometer: {accel_msg.data}, Gyroscope: {gyro_msg.data}')
+
+
 
     def create_pointcloud(self, depth_frame, color_image, color_intrin, frame_id):
         # Get intrinsics
