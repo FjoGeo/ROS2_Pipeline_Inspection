e6b974bf8d4e09b2935f603bf7f6fde595f39f19
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-08-14 16:58:21 +0200

    Update setup.py

--- realsense/my_realsense/setup.py
+++ realsense/my_realsense/setup.py
@@ -1,29 +1,65 @@
 from setuptools import find_packages, setup
 
+
+
 package_name = 'my_realsense'
 
+
+
 setup(
+
     name=package_name,
+
     version='0.0.0',
+
     packages=find_packages(exclude=['test']),
+
     data_files=[
+
         ('share/ament_index/resource_index/packages',
+
             ['resource/' + package_name]),
+
         ('share/' + package_name, ['package.xml']),
+
     ],
+
     install_requires=['setuptools','rclpy', 'sensor_msgs', 'cv_bridge', 'numpy', 'pyrealsense2'],
+
     zip_safe=True,
+
     maintainer='user',
+
     maintainer_email='user@todo.todo',
+
     description='TODO: Package description',
+
     license='TODO: License declaration',
+
     tests_require=['pytest'],
+
     entry_points={
+
         'console_scripts': [
-            'talker = my_realsense.talker:main',
-            'listener_rgb = my_realsense.sub_rgb:main',
-            'listener_depth = my_realsense.sub_depth:main',
+
+            'talker_depth = my_realsense.talker_depth:main',
+
+            'talker_rgb = my_realsense.talker_rgb:main',
+
+            'talker_pc = my_realsense.talker_pc:main',
+
+            'talker_acc_gyro = my_realsense.talker_acc_gyro:main',
+
+            'talker_IR = my_realsense.talker_IR:main',
+
+            'sub_rgb = my_realsense.sub_rgb:main',
+
+            'sub_depth = my_realsense.sub_depth:main',
+
             'listener_pointcloud = my_realsense.sub_pc:main',
+
         ],
+
     },
+
 )
