89001e663c5359fbfad55a93afe0a6a10a987185
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-03 12:09:25 +0100

    Update talker_pc.py

--- realsense/my_realsense/my_realsense/talker_pc.py
+++ realsense/my_realsense/my_realsense/talker_pc.py
@@ -19,7 +19,7 @@ class RealSensePointCloudPublisher(Node):
                 ('pc_topic1', 'realsense1/pointcloud'),
                 ('pc_topic2', 'realsense2/pointcloud'),
                 ('resolution', (640, 480)),
-                ('frame_rate', 30),
+                ('frame_rate', 200), # 30 standard
                 ('publish_rate', 10.0)
             ]
         )
