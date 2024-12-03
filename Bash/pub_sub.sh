a3027cf9e42fe2efbda5b18a48dee1d6d3019f69
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-03 12:57:37 +0100

    Update pub_sub.sh

--- Bash/pub_sub.sh
+++ Bash/pub_sub.sh
@@ -3,7 +3,7 @@
 set -e  # Exit on any command failure
 
 # Launch publisher in the current terminal
-gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run talker_rgb; exec bash" || { echo "Failed to start Publisher"; exit 1; }
+gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run my_realsense talker_rgb; exec bash" || { echo "Failed to start Publisher"; exit 1; }
 
 # Launch subscriber in a new terminal
-gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run sub_rgb; exec bash" || { echo "Failed to start Subscriber"; exit 1; }
+gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run my_realsense sub_rgb; exec bash" || { echo "Failed to start Subscriber"; exit 1; }
