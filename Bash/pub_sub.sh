ab8ae8f51e6ca1aeabb284d392e97015955e0788
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-03 11:00:19 +0100

    Create pub_sub.sh

---
+++ Bash/pub_sub.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -e  # Exit on any command failure
+
+# Launch publisher in the current terminal
+gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run talker_rgb; exec bash" || { echo "Failed to start Publisher"; exit 1; }
+
+# Launch subscriber in a new terminal
+gnome-terminal -- bash -c "cd ~/ros2_ws/ && source install/setup.bash && ros2 run sub_rgb; exec bash" || { echo "Failed to start Subscriber"; exit 1; }
