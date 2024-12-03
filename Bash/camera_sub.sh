4e3e2619ab754e8f386a089543f782713e37ea27
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-03 10:54:03 +0100

    Create camera_sub.sh

---
+++ Bash/camera_sub.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+set -e  # Exit on any command failure
+
+
+cd ~/ros2_ws/ || { echo "Failed to change directory"; exit 1; }
+source install/setup.bash || { echo "Failed to source setup.bash"; exit 1; }
+ros2 run sub_rgb || { echo "Failed to start Subscriber"; exit 1; }
