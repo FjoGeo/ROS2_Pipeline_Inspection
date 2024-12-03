0bfc098eb51d1b0a9a9b1e0b92fd67b30a0fdf5b
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-03 13:00:20 +0100

    Update camera_pub.sh

--- Bash/camera_pub.sh
+++ Bash/camera_pub.sh
@@ -5,4 +5,4 @@ set -e  # Exit on any command failure
 
 cd ~/ros2_ws/ || { echo "Failed to change directory"; exit 1; }
 source install/setup.bash || { echo "Failed to source setup.bash"; exit 1; }
-ros2 run talker_rgb || { echo "Failed to start Subscriber"; exit 1; }
+ros2 run my_realsense talker_rgb || { echo "Failed to start Subscriber"; exit 1; }
