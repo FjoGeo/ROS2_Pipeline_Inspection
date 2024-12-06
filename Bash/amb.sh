d4f6b9a337bb88d2711f2dc21951a8d41344d6e1
FjoGeo <71028200+FjoGeo@users.noreply.github.com>
2024-12-06 10:15:02 +0100

    Create amb.sh

---
+++ Bash/amb.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -e  # Exit on any command failure
+
+
+cd ~/ros2_ws/ || { echo "Failed to change directory"; exit 1; }
+source install/setup.bash || { echo "Failed to source setup.bash"; exit 1; }
+cd ~/ros2_ws/launch/ || { echo "Failed to change directory 2"; exit 1; }
+ros2 launch launch_amb.py
