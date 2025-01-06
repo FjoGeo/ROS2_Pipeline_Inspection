3cd0ebcd37af05eb87f0f813e0894f1934327e14
FjoGeo <fjodorow90@googlemail.com>
2025-01-06 11:52:52 +0100

    bash and launch finished

--- Bash/run_1lidar_rgb_acc_gyro.sh
+++ Bash/run_1lidar_rgb_acc_gyro.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit on any command failure
 
-
+sudo chmod 777 /dev/ttyUSB* || { echo "Failed to set permissions"; exit 1; }
 cd ~/ros2_ws/ || { echo "Failed to change directory"; exit 1; }
 source install/setup.bash || { echo "Failed to source setup.bash"; exit 1; }
 cd ~/ros2_ws/launch/ || { echo "Failed to change directory 2"; exit 1; }
-ros2 launch launch_dual_camera_rgb.py
+ros2 launch 1lidar_rgb_acc_gyro.py || { echo "Failed to start launch file"; exit 1; } 
--- Bash/run_2lidar_IMU_RGB_ACC_GYRO.sh
+++ Bash/run_2lidar_IMU_RGB_ACC_GYRO.sh
@@ -2,8 +2,8 @@
 
 set -e  # Exit on any command failure
 
-
+sudo chmod 777 /dev/ttyUSB* || { echo "Failed to set permissions"; exit 1; }
 cd ~/ros2_ws/ || { echo "Failed to change directory"; exit 1; }
 source install/setup.bash || { echo "Failed to source setup.bash"; exit 1; }
 cd ~/ros2_ws/launch/ || { echo "Failed to change directory 2"; exit 1; }
-ros2 launch launch_amb.py
+ros2 launch 2lidar_IMU_RGB_ACC_GYRO.py || { echo "Failed to start launch file"; exit 1; } 
