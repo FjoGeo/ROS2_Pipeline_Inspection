eb7a92fc97cc2402935f9663c230fa653df9b84a
FjoGeo <fjodorow90@googlemail.com>
2025-01-13 11:16:34 +0100

    added launch and bash files

---
+++ launch/acc_gyro.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import datetime
+
+def generate_launch_description():
+    current_time = datetime.datetime.now()
+
+    return LaunchDescription([
+
+
+        Node(
+            package='my_realsense',
+            executable='talker_acc_gyro',
+            name='my_realsense_talker'
+        ),
+
+
+        ExecuteProcess(
+            cmd=['ros2', 'bag', 'record', '-o', f'bagfile_{current_time}',              
+                    '/realsense1/accel', '/realsense1/gyro',
+                    '/realsense2/accel', '/realsense2/gyro'
+                 
+                ],
+            output='screen'
+        )
+ 
+    ])
